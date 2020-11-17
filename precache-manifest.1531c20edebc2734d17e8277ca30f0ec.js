7a71f09fb8d86f007230204f3b1fe16ea77766c1
Duosora <ergi.duoson@gmail.com>
2020-11-17 22:45:44 +0300

    Updates

---
+++ precache-manifest.1531c20edebc2734d17e8277ca30f0ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e25214db70c66e1728ed6175a472945",
+    "url": "/dhatcher/index.html"
+  },
+  {
+    "revision": "8bed138fae4328a282d2",
+    "url": "/dhatcher/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4b0cf112402fd2bde36e",
+    "url": "/dhatcher/static/css/main.1a85c1ba.chunk.css"
+  },
+  {
+    "revision": "8bed138fae4328a282d2",
+    "url": "/dhatcher/static/js/2.75040616.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dhatcher/static/js/2.75040616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0cf112402fd2bde36e",
+    "url": "/dhatcher/static/js/main.8e1c9629.chunk.js"
+  },
+  {
+    "revision": "bdcdb9b1125c9b384b1d",
+    "url": "/dhatcher/static/js/runtime-main.4e50ad71.js"
+  }
+]);
